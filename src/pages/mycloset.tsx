001fa67485f7c0f47bc968e57c578b22d5ca2a07
Winnie Huang <winnieiwingw@gmail.com>
2023-09-15 15:31:29 +0200

    adjust font size

--- src/pages/mycloset.tsx
+++ src/pages/mycloset.tsx
@@ -76,159 +76,159 @@ const MyCloset = () => {
 
   return (
     <>
-    <div className="overflow-x-auto">
-      <NavBar />
-      <div className="flex justify-center  pt-6">
-        <p>
-          <Lottie
-            className="h-40, w-40 flex flex-grow justify-center items-center"
-            animationData={animationMale}
-          />
-        </p>
-        {/* <span>
+      <div className="overflow-x-auto">
+        <NavBar />
+        <div className="flex justify-center  pt-6">
+          <p>
+            <Lottie
+              className="h-40, w-40 flex flex-grow justify-center items-center"
+              animationData={animationMale}
+            />
+          </p>
+          {/* <span>
           <Lottie
             className="h-20, w-20 items-center"
             animationData={animationquestionMark}
           />
         </span> */}
-        <span>
-          <Lottie
-            className="h-40, w-40 flex items-center "
-            animationData={animationFemale}
-          />
-        </span>
-      </div>
-      <div>
-        <Link
-          className=" flex justify-center  pb-10 text-amber-600 font-semibold underline-offset-8 scroll-m-20 text-xs  tracking-tight lg:text-2xl"
-          href="/"
-        >
-          Check Out Daily Outfit Recommedations
-          <MousePointerClick />
-        </Link>
-      </div>
-      <InputFile />
-      <div>
-        <h3 className="mt-8 scroll-m-20 text-center mb-4 text-2xl font-semibold tracking-tight mx-2 text-yellow-500">
-          Tops
-        </h3>
-        <ul className="flex flex-row  bg-white  p-1 overflow-y-auto space-x-4">
-          {tops.map((item: Closetitems) => {
-            return (
-              <div
-                key={item.id}
-                className="flex flex-row rounded-md border border-yellow-300 shadow mb-4 mx-2"
-              >
-                <Link href={`/mycloset/items/${item.id}`}>
-                  <div>
-                    <div className="w-[100px] text-xs px-2 py-2 text-center bg-yellow-300 text-yellow-900 font-bold">
-                      {item.name}
+          <span>
+            <Lottie
+              className="h-40, w-40 flex items-center "
+              animationData={animationFemale}
+            />
+          </span>
+        </div>
+        <div>
+          <Link
+            className=" flex justify-center  pb-10 text-amber-600 font-semibold underline-offset-8 scroll-m-20 text-xs  tracking-tight lg:text-base"
+            href="/"
+          >
+            Check Out Daily Outfit Recommedations
+            <MousePointerClick />
+          </Link>
+        </div>
+        <InputFile />
+        <div>
+          <h3 className="mt-8 scroll-m-20 text-center mb-4 text-2xl font-semibold tracking-tight mx-2 text-yellow-500">
+            Tops
+          </h3>
+          <ul className="flex flex-row  bg-white  p-1 overflow-y-auto space-x-4">
+            {tops.map((item: Closetitems) => {
+              return (
+                <div
+                  key={item.id}
+                  className="flex flex-row rounded-md border border-yellow-300 shadow mb-4 mx-2"
+                >
+                  <Link href={`/mycloset/items/${item.id}`}>
+                    <div>
+                      <div className="w-[100px] text-xs px-2 py-2 text-center bg-yellow-300 text-yellow-900 font-bold">
+                        {item.name}
+                      </div>
+                      <Image
+                        src={item.imgUrl}
+                        className="rounded-b-md"
+                        alt={`Image of ${item.name}`}
+                        width={100}
+                        height={100}
+                      />
                     </div>
-                    <Image
-                      src={item.imgUrl}
-                      className="rounded-b-md"
-                      alt={`Image of ${item.name}`}
-                      width={100}
-                      height={100}
-                    />
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </ul>
+                  </Link>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+        <div>
+          <h3 className="mt-8 scroll-m-20 text-center mb-4 text-2xl font-semibold tracking-tight mx-2 text-yellow-500">
+            Bottoms
+          </h3>
+          <ul className="flex flex-row  bg-white  p-1 overflow-y-auto space-x-4">
+            {bottoms.map((item: Closetitems) => {
+              return (
+                <div
+                  key={item.id}
+                  className=" flex flex-row rounded-md border border-yellow-300 shadow mb-4 mx-2 "
+                >
+                  <Link href={`/mycloset/items/${item.id}`}>
+                    <div>
+                      <p className="w-[100px] text-xs px-2 py-2 text-center bg-yellow-300 text-yellow-900 font-bold">
+                        {item.name}
+                      </p>
+                      <Image
+                        src={item.imgUrl}
+                        className="rounded-b-md"
+                        alt={`Image of ${item.name}`}
+                        width={100}
+                        height={100}
+                      />
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+        <div>
+          <h3 className="mt-8 scroll-m-20 text-center mb-4 text-2xl font-semibold tracking-tight mx-2 text-yellow-500">
+            Jumpsuits
+          </h3>
+          <ul className="flex flex-row  bg-white  p-1 overflow-y-auto space-x-4">
+            {jumpsuits.map((item: Closetitems) => {
+              return (
+                <div
+                  key={item.id}
+                  className=" flex flex-row rounded-md border border-yellow-300 shadow mb-4 mx-2 "
+                >
+                  <Link href={`/mycloset/items/${item.id}`}>
+                    <div>
+                      <p className="w-[100px] text-xs px-2 py-2 text-center bg-yellow-300 text-yellow-900 font-bold">
+                        {item.name}
+                      </p>
+                      <Image
+                        src={item.imgUrl}
+                        className="rounded-b-md"
+                        alt={`Image of ${item.name}`}
+                        width={100}
+                        height={100}
+                      />
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+        <div>
+          <h3 className="mt-8 scroll-m-20 text-center mb-4 text-2xl font-semibold tracking-tight mx-2 text-yellow-500">
+            Dresses
+          </h3>
+          <ul className="flex flex-row  bg-white  p-1 overflow-y-auto space-x-4">
+            {dresses.map((item: Closetitems) => {
+              return (
+                <div
+                  key={item.id}
+                  className=" flex flex-row rounded-md border border-yellow-300 shadow mb-4 mx-2 "
+                >
+                  <Link href={`/mycloset/items/${item.id}`}>
+                    <div>
+                      <p className="w-[100px] text-xs px-2 py-2 text-center bg-yellow-300 text-yellow-900 font-bold">
+                        {item.name}
+                      </p>
+                      <Image
+                        src={item.imgUrl}
+                        className="rounded-b-md"
+                        alt={`Image of ${item.name}`}
+                        width={100}
+                        height={100}
+                      />
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-      <div>
-        <h3 className="mt-8 scroll-m-20 text-center mb-4 text-2xl font-semibold tracking-tight mx-2 text-yellow-500">
-          Bottoms
-        </h3>
-        <ul className="flex flex-row  bg-white  p-1 overflow-y-auto space-x-4">
-          {bottoms.map((item: Closetitems) => {
-            return (
-              <div
-                key={item.id}
-                className=" flex flex-row rounded-md border border-yellow-300 shadow mb-4 mx-2 "
-              >
-                <Link href={`/mycloset/items/${item.id}`}>
-                  <div>
-                    <p className="w-[100px] text-xs px-2 py-2 text-center bg-yellow-300 text-yellow-900 font-bold">
-                      {item.name}
-                    </p>
-                    <Image
-                      src={item.imgUrl}
-                      className="rounded-b-md"
-                      alt={`Image of ${item.name}`}
-                      width={100}
-                      height={100}
-                    />
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-      <div>
-        <h3 className="mt-8 scroll-m-20 text-center mb-4 text-2xl font-semibold tracking-tight mx-2 text-yellow-500">
-          Jumpsuits
-        </h3>
-        <ul className="flex flex-row  bg-white  p-1 overflow-y-auto space-x-4">
-          {jumpsuits.map((item: Closetitems) => {
-            return (
-              <div
-                key={item.id}
-                className=" flex flex-row rounded-md border border-yellow-300 shadow mb-4 mx-2 "
-              >
-                <Link href={`/mycloset/items/${item.id}`}>
-                  <div>
-                    <p className="w-[100px] text-xs px-2 py-2 text-center bg-yellow-300 text-yellow-900 font-bold">
-                      {item.name}
-                    </p>
-                    <Image
-                      src={item.imgUrl}
-                      className="rounded-b-md"
-                      alt={`Image of ${item.name}`}
-                      width={100}
-                      height={100}
-                    />
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-      <div>
-        <h3 className="mt-8 scroll-m-20 text-center mb-4 text-2xl font-semibold tracking-tight mx-2 text-yellow-500">
-          Dresses
-        </h3>
-        <ul className="flex flex-row  bg-white  p-1 overflow-y-auto space-x-4">
-          {dresses.map((item: Closetitems) => {
-            return (
-              <div
-                key={item.id}
-                className=" flex flex-row rounded-md border border-yellow-300 shadow mb-4 mx-2 "
-              >
-                <Link href={`/mycloset/items/${item.id}`}>
-                  <div>
-                    <p className="w-[100px] text-xs px-2 py-2 text-center bg-yellow-300 text-yellow-900 font-bold">
-                      {item.name}
-                    </p>
-                    <Image
-                      src={item.imgUrl}
-                      className="rounded-b-md"
-                      alt={`Image of ${item.name}`}
-                      width={100}
-                      height={100}
-                    />
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
     </>
   );
 };
